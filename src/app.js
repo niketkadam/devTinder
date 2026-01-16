89deed066e8dd8669880d70efea645608e0e870f
niketk <niketk@azuga.com>
2026-01-16 15:09:42 +0530

    Middleware uses

--- src/app.js
+++ src/app.js
@@ -1,15 +1,24 @@
 const express = require('express');
+const {adminAuth,userAuth} = require('./middlewares/auth');
 const app = express();
 
+app.use("/admin", adminAuth);
 
+app.get("/user/getData",userAuth,(req,res)=>{
+    res.send("get user data");
+})
 
-app.use("/users",(req,res)=>{
-    res.send("users came");
-});
+app.post("/user/login",(req,res)=>{
+    res.send("logged in successfully");
+})
 
-app.use("/",(req,res)=>{
-    res.send("Welocme to node tinder");
- });
+app.get("/admin",(req,res)=>{
+    res.send("user came");
+})
+
+app.get("/admin/deleteuser",(req,res)=>{
+    res.send("deleted userr");
+})
 
 app.listen(3001,()=>{
     console.log('Server is running on port 3001');
