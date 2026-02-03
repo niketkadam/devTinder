4618244bd07245e98e450ae0729860aabb400a71
niketk <niketk@azuga.com>
2026-02-03 19:24:32 +0530

    Dynamic signup api

--- src/app.js
+++ src/app.js
@@ -4,24 +4,25 @@ const connectDb = require("./config/database");
 const app = express();
 const User = require("./models/user");
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
-    const user = new User({
-        firstName: "Kajal",
-        lastName: "Sinha",
-        email: "Kajal@gmail.com",
-        password: "Kajal@123",
-    });
+  const user = new User(req.body);
+  try {
     await user.save();
-    res.send("Data is saved successfully");
+    res.send();
+  } catch (error) {
+    res.status(400).send("error in saving");
+  }
 });
 
 connectDb()
-    .then(() => {
-        console.log("database connected successfully");
-        app.listen(3001, () => {
-            console.log("Server is running on port 3001");
-        });
-    })
-    .catch((err) => {
-        console.log("database not connected");
+  .then(() => {
+    console.log("database connected successfully");
+    app.listen(3001, () => {
+      console.log("Server is running on port 3001");
     });
+  })
+  .catch((err) => {
+    console.log("database not connected");
+  });
